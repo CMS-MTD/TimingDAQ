ae4930701b6ad80c393c4a4a252292916061a099
christina <christiw@fnal.gov>
2025-07-13 19:31:50 -0500

    update script for corryvreckan

--- script.sh
+++ script.sh
@@ -9,10 +9,10 @@ BASE=2025_08_SNSPD
 chmod 755 NetScopeStandaloneDat2Root
 chmod 755 add_branches_TimingDAQ.py
 cp /eos/uscms/store/group/cmstestbeam/${BASE}/LecroyScope/RecoData/ConversionRECO/converted_run$1.root .
-cp /eos/uscms/store/group/cmstestbeam/${BASE}/Tracks/v1/Run$1_CMSTiming_FastTriggerStream_converted.root .
+cp /eos/uscms/store/group/cmstestbeam/${BASE}/Tracks/RecoData/v1/Run$1_CMSTiming_FastTriggerStream_converted.root .
 cp /eos/uscms//store/group/cmstestbeam/${BASE}//ConfigInfo/Runs/info_$1.json .
 ls
-./NetScopeStandaloneDat2Root --input_file=converted_run$1.root --pixel_input_file=/eos/uscms//store/group/cmstestbeam/${BASE}/Tracks/v1/Run$1_CMSTiming_FastTriggerStream_converted.root  --config=LecroyScope_$2.config --output_file=out_run$1.root --save_meas --correctForTimeOffsets=true
+./NetScopeStandaloneDat2Root --input_file=converted_run$1.root --pixel_input_file=Run$1_CMSTiming_FastTriggerStream_converted.root  --config=LecroyScope_$2.config --output_file=out_run$1.root --save_meas --correctForTimeOffsets=true
 python3 add_branches_TimingDAQ.py $1 9999 out_run$1.root
 ls
 xrdcp -fs out_run$1_info.root root://cmseos.fnal.gov//store/group/cmstestbeam/${BASE}/LecroyScope/RecoData/TimingDAQRECO/RecoWithTracks/$2//run$1_info.root
